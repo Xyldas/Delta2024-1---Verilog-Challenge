504ad6204641cd6ca57e665d8a26fa9102763cd5
Xyldas <165372764+Xyldas@users.noreply.github.com>
2024-06-28 16:41:52 +0300

    Add files via upload

---
+++ Week 1/D9 - MUX and DFF/muxdff.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+
+    wire D;
+    assign D = L ? r_in : q_in;
+    
+    always@(posedge clk) begin
+        Q <= D;
+    end
+    
+endmodule
